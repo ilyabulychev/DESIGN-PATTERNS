efbd17d88ccf962abaee1a7c5c57001528e2fb48
ilyabulychev <info@ilyabulychev.com>
2018-02-20 04:58:27 +0300

    DIP Design Pattern
    DIP Design Pattern

--- DESIGN-PRINCIPLES/DIP/DependencyInversion.ts
+++ DESIGN-PRINCIPLES/DIP/DependencyInversion.ts
@@ -1,13 +1,56 @@
 namespace DependencyInversion {
 
     /// <summary>
+    /// Printer Interface
+    /// </summary>
+    export interface IPrinter {
+        Print();
+        Clean();
+    }
+
+    /// <summary>
+    /// HP Printer
+    /// </summary>
+    export class HPPrinter implements IPrinter {
+        Print() {
+            console.log(`HP printer is printing a paper...`);
+        }
+        Clean() {
+            console.log(`HP printer is cleaning...`);
+        }
+    }
+
+    /// <summary>
+    /// Canon Printer
+    /// </summary>
+    export class CanonPrinter implements IPrinter {
+        Print() {
+            console.log(`Canon printer is printing a paper...`);
+        }
+        Clean() {
+            console.log(`Canon printer is cleaning...`);
+        }
+    }
+
+    /// <summary>
+    /// Report Manager
+    /// </summary>
+    export class ReportManager {
+        Print(printer: IPrinter) {
+            printer.Print();
+        }
+    }
+
+    /// <summary>
     /// Application
     /// </summary>
     export class Application {
         
         static Main() 
         {
-          
+            let report = new ReportManager();
+            // We can change the printer to any different printers
+            report.Print(new CanonPrinter());
         }
     }
 
